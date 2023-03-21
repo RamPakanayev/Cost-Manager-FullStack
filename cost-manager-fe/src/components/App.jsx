a358fd18f6a9c13ace944287a065c89bb0864174
Ram Pakanayev <rampakanayev@gmail.com>
2023-03-21 23:41:35 +0200

    remove the local storage usage

--- cost-manager-fe/src/components/App.jsx
+++ cost-manager-fe/src/components/App.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import LocalStorageLibrary from "../local-storage";
 import AddCostForm from "./AddCostForm";
 import ReportsForm from "./ReportsForm";
 
@@ -18,52 +17,16 @@ useEffect(() => {
   setImagePath(process.env.PUBLIC_URL + "/Logo.png");
 }, []); // this will make the effect to be called only once when the component is rendered
 
-// callback function that adds a new cost item to the local storage
-// It uses the LocalStorageLibrary tosave the new cost item
-async function handleAddCostItem(sum, category, description,date) {
-  try {
-    if (category === "" || category === "Select a category")
-      category = "Other";
-    if (sum === "")
-      window.alert("Please enter a sum");
-    else if (description === "")
-      window.alert("Please enter a description");
-    else {
-      await LocalStorageLibrary.addCostItem({
-        sum,
-        category,
-        description,
-        date,
-      });
-      window.alert("The new cost has successfully added !");
-    }
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-// callback function that gets a report of the costs based on the selected month and year
-// It uses the LocalStorageLibrary to get the report data and updates the report and showReport state variables
-async function handleGetReport(month, year) {
-  try {
-    const reportData = await LocalStorageLibrary.getReport(month, year);
-    setReport(reportData);
-    setShowReport(true);
-  } catch (error) {
-    console.error(error);
-  }
-}
 
 return (
   <div className="App">
     <img src={imagePath} alt="Logo" />
-    <AddCostForm handleAddCostItem={handleAddCostItem} />
+    <AddCostForm  />
     <ReportsForm
       month={month}
       setMonth={setMonth}
       year={year}
       setYear={setYear}
-      handleGetReport={handleGetReport}
       showReport={showReport}
       report={report}
     />
