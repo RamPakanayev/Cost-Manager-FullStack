7813b195166f5e8fc151d63582b48fd0ef5c7aea
Ram Pakanayev <rampakanayev@gmail.com>
2023-03-21 23:05:19 +0200

    connecting the reports to the backend API

--- cost-manager-fe/src/components/Report.jsx
+++ cost-manager-fe/src/components/Report.jsx
@@ -1,49 +1,61 @@
 import React from "react";
 
-// A functional component that receives an object 'reportData' as a prop.
-// It calculates and displays the total sum of all the report items and displays each report item's sum, category, description, and date.
-// If no report data is found, it displays a message 'No Report data Found.'
 function Report({ reportData }) {
-  // A callback function that calculates the total sum of all the report items.
-  // It loops through the reportData array and adds each item's 'sum' value to the total sum.
-  // It then returns the total sum formatted as a US currency.
   const totalSum = () => {
     let sum = 0;
-    reportData.forEach((item) => {
-      sum += Number(item.sum);
+    Object.values(reportData).forEach((category) => {
+      if (Array.isArray(category)) {
+        category.forEach((item) => {
+          sum += Number(item.sum);
+        });
+      }
     });
     return new Intl.NumberFormat("en-US").format(sum);
   };
+  
+
+  const nonEmptyCategories = Object.entries(reportData)
+    .filter(([, categoryData]) => categoryData.length > 0)
+    .map(([category, categoryData]) => ({ category, categoryData }));
+
+  const sortedCategories = nonEmptyCategories.map(({ category, categoryData }) => ({
+    category,
+    categoryData: categoryData.sort((a, b) => new Date(a.date) - new Date(b.date)),
+  }));
 
   return (
     <>
-      <div className="reports">
-        {reportData.length > 0 ? (
-          reportData.map((item, index) => (
-            <div key={index} className="report">
-              <div className="ReportNumber">
-                <p className="pCenter">{index + 1}</p>
-              </div>
-              <p className="bold">
-                Sum: {Intl.NumberFormat("en-US").format(item.sum)} $
-              </p>
-              <p>Category: {item.category}</p>
-              <p>Description: {item.description}</p>
-              {/* Use the toLocaleString method to format the date */}
-              <p>
-                Date:{" "}
-                {new Date(item.date).toLocaleString("en-GB", {
-                  year: "numeric",
-                  month: "2-digit",
-                  day: "2-digit",
-                  hour: "2-digit",
-                  minute: "2-digit",
-                  second: "2-digit"
-                })}
-              </p>
+      <div >
+        {sortedCategories.map(({ category, categoryData }) => (
+          <React.Fragment key={category}>
+            <h3>{category}</h3>
+            <div className="reports">
+              {categoryData.map((item, index) => (
+                <div key={index} className="report">
+                  <div className="ReportNumber">
+                    <p className="pCenter">{index + 1}</p>
+                  </div>
+                  <p className="bold">
+                    Sum: {Intl.NumberFormat("en-US").format(item.sum)} $
+                  </p>
+                  <p>Description: {item.description}</p>
+                  <p>
+                    Date:{" "}
+                    {new Date(item.date).toLocaleString("en-GB", {
+                      year: "numeric",
+                      month: "2-digit",
+                      day: "2-digit",
+                      hour: "2-digit",
+                      minute: "2-digit",
+                      second: "2-digit",
+                    })}
+                  </p>
+                </div>
+              ))}
             </div>
-          ))
-        ) : (
+          </React.Fragment>
+        ))}
+        {nonEmptyCategories.length === 0 && (
           <div className="no-data-found">No Report data Found.</div>
         )}
         <div className="total-sum">Total Sum: {totalSum()} $</div>
