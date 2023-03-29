2dd7b598bd7e986ea8039b48fc711e576dc40159
Ram Pakanayev <rampakanayev@gmail.com>
2023-03-30 01:45:31 +0300

    header style

--- cost-manager-fe/src/components/Header/Header.jsx
+++ cost-manager-fe/src/components/Header/Header.jsx
@@ -1,18 +1,28 @@
-import React, { useState, useEffect } from 'react';
-import './Header.css';
+import React, { useState, useEffect } from "react";
+import "./Header.css";
 
-function Header({handleLogout }) {
+function Header({ handleLogout }) {
   return (
     <header className="header">
       <div className="logo-container">
-        <img src="../../../Logo.png" alt="Logo" className="logo" href="/"/>
+        <img src="../../../Logo.png" alt="Logo" className="logo" href="/" />
       </div>
       <nav className="nav-links">
-        <a href="/homepage" className="nav-link">Home</a>
-        <a href="/user-info" className="nav-link">User Info</a>
-        <a href="/settings" className="nav-link">Settings</a>
-        <a href="/homepage" className="nav-link">Search</a>
-        <a href="/settings" className="nav-link">About</a>
+        <a href="/homepage" className="nav-link">
+          Home
+        </a>
+        <a href="/user-info" className="nav-link">
+          Profile
+        </a>
+        <a href="/settings" className="nav-link">
+          Settings
+        </a>
+        <a href="/homepage" className="nav-link">
+          Search
+        </a>
+        <a href="/settings" className="nav-link">
+          About
+        </a>
         <button onClick={handleLogout}>Logout</button>
       </nav>
     </header>
