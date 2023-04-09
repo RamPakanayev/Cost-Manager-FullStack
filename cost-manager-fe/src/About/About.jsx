534d8208eb3bd704eb10154d6ea2016bf5aa6a66
Ram Pakanayev <rampakanayev@gmail.com>
2023-04-09 21:20:47 +0300

    update about links

--- cost-manager-fe/src/About/About.jsx
+++ cost-manager-fe/src/About/About.jsx
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from "react";
-// import "./About.css";
+import "./About.css";
+import gitHubLogo from './logos/github_logo.png'; // Import GitHub logo
+import linkedInLogo from './logos/linkedin_logo.png'; // Import LinkedIn logo
+import emailLogo from './logos/email_logo.png'; // Import Email logo
 
 function About() {
   const [developers, setDevelopers] = useState([]);
-
+   const width=100;
+   const height=24;
   useEffect(() => {
     const fetchDevelopers = async () => {
       try {
@@ -23,42 +27,49 @@ function About() {
   }, []);
 
   return (
-    <form className="about">
+    <div className="about">
       <h2>About the Developer:</h2>
       <hr />
 
-      <ul className="developer-list">
-        {developers.map((developer) => (
-          <li key={developer.id}>
-            <p>Name: {developer.firstName} {developer.lastName}</p>
-            <p>
-              GitHub:{" "}
-              <a href={developer.gitHubUrl} target="_blank" rel="noopener noreferrer">
-                {developer.gitHubUrl}
-              </a>
-            </p>
-            <p>
-              Project GitHub Url:{" "}
-              <a href={developer.projectUrl} target="_blank" rel="noopener noreferrer">
-                {developer.projectUrl}
-              </a>
-            </p>
-            <p>
-              Linkedin:{" "}
-              <a href={developer.LinkedinUrl} target="_blank" rel="noopener noreferrer">
-                {developer.LinkedinUrl}
-              </a>
-            </p>
-            <p>
-              Email:{" "}
-              <a href={`mailto:${developer.email}`} target="_blank" rel="noopener noreferrer">
-                {developer.email}
-              </a>
-            </p>
-          </li>
-        ))}
-      </ul>
-    </form>
+      <table className="developer-table">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>GitHub</th>
+            <th>Project GitHub Url</th>
+            <th>LinkedIn</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {developers.map((developer) => (
+            <tr key={developer.id}>
+              <td>{developer.firstName} {developer.lastName}</td>
+              <td>
+                <a href={developer.gitHubUrl} target="_blank" rel="noopener noreferrer">
+                  <img src={gitHubLogo} alt="GitHub" width={width} height={height} />
+                </a>
+              </td>
+              <td>
+                <a href={developer.projectUrl} target="_blank" rel="noopener noreferrer">
+                  <img src={gitHubLogo} alt="Project GitHub Url" width={width} height={height} />
+                </a>
+              </td>
+              <td>
+                <a href={developer.LinkedinUrl} target="_blank" rel="noopener noreferrer">
+                  <img src={linkedInLogo} alt="LinkedIn" width={width} height={height} />
+                </a>
+              </td>
+              <td>
+                <a href={`mailto:${developer.email}`} target="_blank" rel="noopener noreferrer">
+                  <img src={emailLogo} alt="Email" width={width} height={height} />
+                </a>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
 
