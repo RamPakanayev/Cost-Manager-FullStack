73a3267f44e47fc1558cb4e9644af971e8ec7746
Ram Pakanayev <rampakanayev@gmail.com>
2023-04-10 00:38:52 +0300

    style

--- cost-manager-fe/src/About/About.jsx
+++ cost-manager-fe/src/About/About.jsx
@@ -3,6 +3,7 @@ import "./About.css";
 import gitHubLogo from './logos/github_logo.png'; // Import GitHub logo
 import linkedInLogo from './logos/linkedin_logo.png'; // Import LinkedIn logo
 import emailLogo from './logos/email_logo.png'; // Import Email logo
+import ramLogo from './logos/RamPic.jpg';
 
 function About() {
   const [developers, setDevelopers] = useState([]);
@@ -28,15 +29,15 @@ function About() {
 
   return (
     <div className="about">
-      <h2>About the Developer:</h2>
+      <h2>About:</h2>
       <hr />
 
       <table className="developer-table">
         <thead>
           <tr>
-            <th>Name</th>
-            <th>GitHub</th>
-            <th>Project GitHub Url</th>
+            <th>Developer</th>
+            
+            <th>GitHub Project</th>
             <th>LinkedIn</th>
             <th>Email</th>
           </tr>
@@ -44,20 +45,20 @@ function About() {
         <tbody>
           {developers.map((developer) => (
             <tr key={developer.id}>
-              <td>{developer.firstName} {developer.lastName}</td>
               <td>
-                <a href={developer.gitHubUrl} target="_blank" rel="noopener noreferrer">
-                  <img src={gitHubLogo} alt="GitHub" width={width} height={height} />
-                </a>
+              
+              <a href={developer.gitHubUrl} target="_blank" rel="noopener noreferrer">
+              <img src={ramLogo} alt="RamLogo" width={width} height={height} /></a>
               </td>
+        
               <td>
                 <a href={developer.projectUrl} target="_blank" rel="noopener noreferrer">
                   <img src={gitHubLogo} alt="Project GitHub Url" width={width} height={height} />
                 </a>
               </td>
               <td>
-                <a href={developer.LinkedinUrl} target="_blank" rel="noopener noreferrer">
-                  <img src={linkedInLogo} alt="LinkedIn" width={width} height={height} />
+                <a  href={developer.LinkedinUrl} target="_blank" rel="noopener noreferrer">
+                  <img className="linkedin" src={linkedInLogo} alt="LinkedIn" width={width} height={height} />
                 </a>
               </td>
               <td>
